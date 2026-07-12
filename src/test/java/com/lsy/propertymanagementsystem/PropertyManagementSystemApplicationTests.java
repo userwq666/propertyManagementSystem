44dd793cac361b8386e5999a2e90e535c9cc5f88
Admin <admin@property.com>
2026-07-12 23:52:41 +0800

    feat: 创建数据库表结构和初始数据

---
+++ src/test/java/com/lsy/propertymanagementsystem/PropertyManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsy.propertymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
