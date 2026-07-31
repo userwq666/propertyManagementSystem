f6733ddfa8a78029d1858269f808ac9322385a40
Admin <admin@property.com>
2026-08-01 02:02:57 +0800

    style: 设备台账操作按钮放一行

--- frontend/src/views/equipment/equipment/index.vue
+++ frontend/src/views/equipment/equipment/index.vue
@@ -40,7 +40,7 @@
           </template>
         </el-table-column>
         <el-table-column prop="createTime" label="创建时间" width="180" />
-        <el-table-column label="操作" min-width="240" class-name="action-column" fixed="right">
+        <el-table-column label="操作" min-width="320" class-name="action-column" fixed="right">
           <template #default="{ row }">
             <el-button size="small" @click="handleDetail(row)" v-permission="'equipment:list:list'">详情</el-button>
             <el-button type="primary" size="small" @click="handleEdit(row)" v-permission="'equipment:list:edit'">编辑</el-button>
