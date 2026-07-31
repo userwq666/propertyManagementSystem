6c201e08b15df8046c6779f08499bc91a26282ae
Admin <admin@property.com>
2026-07-31 23:46:06 +0800

    style: 巡检计划操作按钮单行展示

--- frontend/src/views/inspection/plan/index.vue
+++ frontend/src/views/inspection/plan/index.vue
@@ -37,7 +37,7 @@
         </el-table-column>
         <el-table-column prop="creatorName" label="创建人" width="80" />
         <el-table-column prop="createTime" label="创建时间" width="160" />
-        <el-table-column label="操作" min-width="240" class-name="action-column" fixed="right">
+        <el-table-column label="操作" min-width="320" class-name="action-column" fixed="right">
           <template #default="{ row }">
             <el-button size="small" @click="handleDetail(row)">详情</el-button>
             <el-button type="primary" size="small" @click="handleEdit(row)" v-permission="'inspection:plan:edit'">编辑</el-button>
